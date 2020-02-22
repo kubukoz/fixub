7e1b78de0d6bc4bee9be2ac563f3cb721796b66e
Jakub Kozłowski <kubukoz@gmail.com>
2020-02-22 16:45:14 +0100

    Update libs, add native image

--- build.sbt
+++ build.sbt
@@ -2,12 +2,13 @@ name := "fixub"
 
 version := "1.0"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.12.10"
 
 libraryDependencies ++= Seq(
-  "co.fs2" %% "fs2-io" % "1.1.0-M1",
-  "dev.profunktor" %% "console4cats" % "0.8.0-M1",
-  "org.scodec" %% "scodec-stream" % "1.2.1",
+  "co.fs2" %% "fs2-io" % "2.2.2",
+  "dev.profunktor" %% "console4cats" % "0.8.1",
+  "org.scodec" %% "scodec-stream" % "2.0.0",
 )
 
 enablePlugins(JavaAppPackaging)
+enablePlugins(GraalVMNativeImagePlugin)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,6 +12,7 @@ import scodec.Decoder
 import scodec.Err
 import scodec.GenCodec
 import scodec.bits.BitVector
+import scodec.stream.StreamDecoder
 
 object Main extends IOApp {
   val ChunkSize = 4096
@@ -23,15 +24,12 @@ object Main extends IOApp {
     args match {
       case srcFileName :: destFileName :: remainingArgs =>
         val codec: IO[Decoder[String]] =
-          remainingArgs.headOption
-            .map(name => IO(Charset.forName(name)))
-            .getOrElse(DefaultInputEncoding.pure[IO])
-            .map {
-              scodec.codecs.string(_).emap {
-                case "" => Attempt.failure(Err("Empty string"))
-                case s  => Attempt.successful(s)
-              }
+          remainingArgs.headOption.map(name => IO(Charset.forName(name))).getOrElse(DefaultInputEncoding.pure[IO]).map {
+            scodec.codecs.string(_).emap {
+              case "" => Attempt.failure(Err("Empty string"))
+              case s  => Attempt.successful(s)
             }
+          }
 
         Blocker[IO].use { implicit blocker =>
           codec.flatMap { implicit c =>
@@ -43,14 +41,16 @@ object Main extends IOApp {
     }
 
   private def convertAndSave(from: String, to: String)(implicit blocker: Blocker, stringDecoder: Decoder[String]) =
-    fs2.io.file
+    fs2
+      .io
+      .file
       .readAll[IO](
         Paths.get(from),
         blocker,
         chunkSize = ChunkSize
       )
       .through(bytesToBitVectors)
-      .through(scodec.stream.decode.pipe[IO, String])
+      .through(StreamDecoder.many(stringDecoder).toPipe[IO])
       .through(fs2.text.utf8Encode[IO])
       .through(fs2.io.file.writeAll[IO](Paths.get(to), blocker))
 }
